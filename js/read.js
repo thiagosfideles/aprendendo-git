090c3490e60c3168c072deef7a321509dfa28d8f
EC2 Default User <ec2-user@ip-172-31-26-30.us-east-2.compute.internal>
2019-10-11 12:05:39 +0000

    alteração no modal de visualização dos eventos

--- js/read.js
+++ js/read.js
@@ -1,14 +1,17 @@
 // Lendo do Bando de Dados
-firebase.database().ref().on("child_added", function (snapshot, prevChildKey) {
+/*firebase.database().ref().on("child_added", function (snapshot, prevChildKey) {
   var newAula = snapshot.val();
   console.log("titulo: " + newAula.title);
   console.log("Data: " + newAula.start);
   console.log("ID: " + prevChildKey);
-});
+});*/
 
-firebase.database().ref('cursos').on('value', function (snapshot) {
+firebase.database().ref('cursos').once('value', function (snapshot) {
  
   snapshot.forEach(function (item) {
-      console.log(item.val().title);
+      console.log("disciplina:" + item.val().title);
+      console.log("data:" + item.val().start);
+      console.log("professor:" + item.val().professor);
+      console.log("sala de aula:" + item.val().salaAula);
   });
 });
