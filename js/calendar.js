2d246198a9c276d776c21b7f70c95f6fa58b7eeb
Thiago Fideles <ec2-user@ip-172-31-26-30.us-east-2.compute.internal>
2019-10-25 13:38:58 +0000

    add format pt date

--- js/calendar.js
+++ js/calendar.js
@@ -19,17 +19,17 @@ document.addEventListener('DOMContentLoaded', function () {
         events: 'https://sigead.firebaseio.com/aulas.json',
 
         eventClick: function(info) {
-                $('#viewEvento #viewDataAula').val(info.event.start);
+                let date = new Date(info.event.start);
+                $('#viewEvento #viewDataAula').val(date.toLocaleDateString());
                 $('#viewEvento #viewCurso').val(info.event.extendedProps.curso);
                 $('#viewEvento #viewModulo').val(info.event.extendedProps.modulo);
+                $('#viewEvento #viewTurma').val(info.event.extendedProps.turma);
                 $('#viewEvento #viewDisciplina').val(info.event.title);
                 $('#viewEvento #viewProfessor').val(info.event.extendedProps.professor);
                 $('#viewEvento #viewSalaVirtual').val(info.event.extendedProps.salaVirtual);
                 $('#viewEvento #viewSalaAula').val(info.event.extendedProps.salaAula);
                 $('#viewEvento #viewObs').val(info.event.extendedProps.observacao);
                 $('#viewEvento').modal('show');
-                        
-                        
         } ,
 
     });
