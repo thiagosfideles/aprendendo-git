d8b37d4d4874088b1c18e1b4f5661c464a69aaaf
Thiago Fideles <ec2-user@ip-172-31-26-30.us-east-2.compute.internal>
2019-11-13 11:32:58 +0000

    alter layout charts

--- js/chart.js
+++ js/chart.js
@@ -17,16 +17,21 @@ window.onload = function () {
 //criando construtor datasets
 let graficoAlunosData = {
     labels: [],
-    datasets: [{
-        label: 'Matriculados',
-        backgroundColor: 'rgba(31,171,137, 1)',
-        borderColor: 'rgba(31,171,137, 0.3)',
-        borderWidth: 1,
-        data: []
-    }, {
+    datasets: [ {
+        type: 'line',
         label: 'Desistentes',
-        backgroundColor: 'rgba(255, 128, 128, 1)',
-        borderColor: 'rgba(55, 128, 128, 0.3)',
+        fill: false,
+        backgroundColor: 'rgba(255, 128, 128, 0.3)',
+        borderColor: 'rgba(255, 128, 128, 1)',
+        borderWidth: 2,
+        data: [],
+        order: 1
+        },
+        {
+        type: 'bar',
+        label: 'Matriculados',
+        backgroundColor: 'rgba(31,171,137, 0.3)',
+        borderColor: 'rgba(31,171,137, 1)',
         borderWidth: 1,
         data: []
     }]
@@ -36,14 +41,35 @@ let graficoAptosData = {
     labels: [],
     datasets: [{
         label: 'Aptos',
-        //backgroundColor: 'rgba(31,171,137, 1)',
-        //borderColor: 'rgba(31,171,137, 0.3)',
+        backgroundColor: 'rgba(31,171,137, 0.3)',
+        borderColor: 'rgba(31,171,137, 1)',
         borderWidth: 1,
         data: []
     }, {
         label: 'Não aptos',
-        //backgroundColor: 'rgba(255, 128, 128, 1)',
-        //borderColor: 'rgba(55, 128, 128, 0.3)',
+        backgroundColor: 'rgba(255, 128, 128, 0.3)',
+        borderColor: 'rgba(255, 128, 128, 1)',
+        borderWidth: 1,
+        data: []
+    }]
+};
+
+let graficoResultadoData = {
+    labels: [],
+    datasets: [ {
+        type: 'line',
+        label: 'Aptos',
+        fill: false,
+        backgroundColor: 'rgba(31, 135, 171, 0.3)',
+        borderColor: 'rgba(31, 135, 171, 1)',
+        borderWidth: 2,
+        data: [],
+        },
+        {
+        type: 'bar',
+        label: 'Matriculados',
+        backgroundColor: 'rgba(31,171,137, 0.3)',
+        borderColor: 'rgba(31,171,137, 1)',
         borderWidth: 1,
         data: []
     }]
@@ -53,9 +79,17 @@ let graficoAptosData = {
 window.onload = async function () {
     const ctx = document.getElementById('graficosAlunos').getContext('2d');
     window.graficosAlunos = new Chart(ctx, {
-        type: 'horizontalBar',
+        type: 'bar',
         data: graficoAlunosData,
         options: {
+            resposive: true,
+            legend: {
+                position: 'right'
+            },
+            tooltips: {
+              mode: 'index',
+              intersect: true
+            },
             scales: {
                 xAxes: [{
                     gridLines: {
@@ -74,9 +108,10 @@ window.onload = async function () {
 
     const ctx1 = document.getElementById('graficosAptos').getContext('2d');
     window.graficosAptos = new Chart(ctx1, {
-        type: 'line',
+        type: 'horizontalBar',
         data: graficoAptosData,
         options: {
+            resposive: true,
             legend: {
                 position: 'right'
             },
@@ -95,6 +130,36 @@ window.onload = async function () {
         }
 
     });
+
+    const ctx2 = document.getElementById('graficosResultado').getContext('2d');
+    window.graficosResultado = new Chart(ctx2, {
+        type: 'bar',
+        data: graficoResultadoData,
+        options: {
+            resposive: true,
+            legend: {
+                position: 'right'
+            },
+            tooltips: {
+              mode: 'index',
+              intersect: true
+            },
+            scales: {
+                xAxes: [{
+                    gridLines: {
+                        display: false,
+                    }
+                }],
+                yAxes: [{
+                    gridLines: {
+                        display: false,
+                    }
+                }],
+            }
+        }
+
+    });
+
 }
 
 //coletando dados para utilzar no grafico
@@ -126,20 +191,41 @@ async function preencheGrafico() {
     if (graficoAptosData.datasets[1].data.length > 0) {
         graficoAptosData.datasets[1].data = []
     }
+    
+    if (graficoResultadoData.labels.length > 0) {
+        graficoResultadoData.labels = []
+    }
+
+    if (graficoResultadoData.datasets[0].data.length > 0) {
+        graficoResultadoData.datasets[0].data = []
+    }
+
+    if (graficoResultadoData.datasets[1].data.length > 0) {
+        graficoResultadoData.datasets[1].data = []
+    }
+    
+    
+    
     dadosRecebidos.forEach(dadoRecebido => {
         if (dadoRecebido.modulo === moduloSelecionado) {
 
             graficoAlunosData.labels.push(dadoRecebido.nome);
-            graficoAlunosData.datasets[0].data.push(dadoRecebido.matriculados);
-            graficoAlunosData.datasets[1].data.push(dadoRecebido.desistentes);
+            graficoAlunosData.datasets[0].data.push(dadoRecebido.desistentes);
+            graficoAlunosData.datasets[1].data.push(dadoRecebido.matriculados);
 
             graficoAptosData.labels.push(dadoRecebido.nome);
             graficoAptosData.datasets[0].data.push(dadoRecebido.aptos);
             graficoAptosData.datasets[1].data.push(dadoRecebido.naoAptos.total);
+            
+            graficoResultadoData.labels.push(dadoRecebido.nome);
+            graficoResultadoData.datasets[0].data.push(dadoRecebido.aptos);
+            graficoResultadoData.datasets[1].data.push(dadoRecebido.matriculados);
+            
 
         }
 
     });
     window.graficosAlunos.update();
     window.graficosAptos.update();
+    window.graficosResultado.update();
 }
